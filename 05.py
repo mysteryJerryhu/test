1094d53b23a3534f9b83e9f698449f5b216e6b31
jerryhu <linxhu@live.com>
2017-08-11 10:11:34 +0800

    Update 05.py

--- 05.py
+++ 05.py
@@ -5,7 +5,7 @@ a = int(random.uniform(1, 7))  # 随机数（强制转换为整型）
 print a
 
 input("b=")
-raw_input("b=")  # 输入语句input（）个性输入，raw_input()统一看为字符串输入
+raw_input("b=")  # 输入语句input（）数值输入，raw_input()为字符串输入
 
 print input("b=")  # 输入输出1
 
